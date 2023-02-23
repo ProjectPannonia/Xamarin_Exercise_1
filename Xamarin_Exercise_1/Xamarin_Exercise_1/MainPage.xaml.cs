76bda6c4a8199a911a03b908491f66d5493976ad
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-02-23 22:01:50 +0100

    Add project files.

---
+++ Xamarin_Exercise_1/Xamarin_Exercise_1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Exercise_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public string[] quotes;
+        public MainPage()
+        {
+            InitializeComponent();
+            slider.Value = 16;
+            quotes = new string[]
+            {
+            "You can't blame gravity for falling in love.",
+            "Qoute 2",
+            "Qoute 3"
+            };
+            quoteLabel.Text = quotes[0];    
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
